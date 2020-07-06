668c5209d7827536c33185076b8b541bab3a8137
zhuying <zy672372>
2020-07-06 12:51:44 +0800

    flag工具替换为cobra工具

--- go.mod
+++ go.mod
@@ -5,12 +5,22 @@ go 1.13
 require (
 	github.com/docker/distribution v2.7.1+incompatible // indirect
 	github.com/eapache/go-resiliency v1.2.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/ghodss/yaml v1.0.0
+	github.com/mitchellh/mapstructure v1.3.2 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/pelletier/go-toml v1.8.0 // indirect
 	github.com/rs/zerolog v1.19.0
-	github.com/smartystreets/goconvey v1.6.4 // indirect
+	github.com/spf13/afero v1.3.1 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/cobra v1.0.0
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
+	golang.org/x/text v0.3.3 // indirect
 	golang.org/x/tools v0.0.0-20200702044944-0cc1aa72b347 // indirect
 	golang.zx2c4.com/wireguard v0.0.20200320 // indirect
 	gopkg.in/ini.v1 v1.57.0 // indirect
-	gopkg.in/yaml.v2 v2.2.7
+	gopkg.in/yaml.v2 v2.3.0
 )
--- main.go
+++ main.go
@@ -1,106 +1,125 @@
 package main
 
 import (
-	"flag"
 	"grandanno/cnv"
 	"grandanno/data"
 	"grandanno/snv"
-	"os"
 	"path"
+
+	"github.com/spf13/cobra"
 )
 
 // Param 参数
 var Param struct {
-	Program        string
 	Input          string
 	Ouput          string
 	Config         string
 	DBPath         string
 	SplicingLength int
-	Help           bool
 }
 
-// runPrepare 数据文件预处理
-func runPrepare() {
-	referenceChan := make(chan data.Fasta, 0)
-	go data.ReadFastaFile(path.Join(Param.DBPath, data.Config.DBFile.Reference), referenceChan)
-	refgenesChan := make(chan data.Refgenes, 0)
-	refgeneFiles := []string{path.Join(Param.DBPath, data.Config.DBFile.Refgene), path.Join(Param.DBPath, data.Config.DBFile.EnsMt)}
-	go data.ReadRefgeneFiles(refgeneFiles, refgenesChan)
-	refidxMapChan := make(chan map[string]data.Refidxs, 0)
-	go data.InitRefidxMap(refidxMapChan)
-	refgenes := <-refgenesChan
-	data.WriteMrnaFile(path.Join(Param.DBPath, data.Config.DBFile.Mrna), refgenes, <-referenceChan)
-	data.WriteRefidxFile(path.Join(Param.DBPath, data.Config.DBFile.Refidx), <-refidxMapChan, refgenes.ToChromMap())
-}
+// CorbaCMD 命令行参数解析
+var CorbaCMD *cobra.Command
 
-// runPrepare 注释GATK SNV
-func runAnnoGatkSnv() {
-	ncbiGeneChan := make(chan data.NcbiGene, 0)
-	go data.ReadNCBIGeneInfo(path.Join(Param.DBPath, data.Config.DBFile.NcbiGene), ncbiGeneChan)
-	refgenesChan := make(chan data.Refgenes, 0)
-	refgeneFiles := []string{path.Join(Param.DBPath, data.Config.DBFile.Refgene), path.Join(Param.DBPath, data.Config.DBFile.EnsMt)}
-	go data.ReadRefgeneFiles(refgeneFiles, refgenesChan)
-	mrnaChan := make(chan data.Fasta, 0)
-	go data.ReadFastaFile(path.Join(Param.DBPath, data.Config.DBFile.Mrna), mrnaChan)
-	refidxsChan := make(chan data.Refidxs)
-	go data.ReadRefidxFile(path.Join(Param.DBPath, data.Config.DBFile.Refidx), refidxsChan)
-	refgenes := <-refgenesChan
-	refgenes.SetEntrezidAndSequence(<-ncbiGeneChan, <-mrnaChan)
-	gatkSnvsChan := make(chan snv.Snvs)
-	go snv.ReadGatkVcfFile(Param.Input, gatkSnvsChan)
-	snv.RunAnnotation(<-gatkSnvsChan, refgenes.ToSnMap(), <-refidxsChan, Param.Ouput)
-}
-
-// runPrepare 注释XHMM CNV
-func runAnnoXhmmCnv() {
-	ncbiGeneChan := make(chan data.NcbiGene, 0)
-	go data.ReadNCBIGeneInfo(path.Join(Param.DBPath, data.Config.DBFile.NcbiGene), ncbiGeneChan)
-	refgenesChan := make(chan data.Refgenes, 0)
-	refgeneFiles := []string{path.Join(Param.DBPath, data.Config.DBFile.Refgene), path.Join(Param.DBPath, data.Config.DBFile.EnsMt)}
-	go data.ReadRefgeneFiles(refgeneFiles, refgenesChan)
-	refidxsChan := make(chan data.Refidxs)
-	go data.ReadRefidxFile(path.Join(Param.DBPath, data.Config.DBFile.Refidx), refidxsChan)
-	xhmmCnvMapChan := make(chan map[string]cnv.Cnvs, 0)
-	go cnv.ReadXhmmVcfFile(Param.Input, xhmmCnvMapChan)
-	refgenes := <-refgenesChan
-	refidxs := <-refidxsChan
-	for sample, cnvs := range <-xhmmCnvMapChan {
-		outJSONFile := path.Join(Param.Ouput + "." + sample + ".json")
-		cnv.RunAnnotation(cnvs, refgenes.ToSnMap(), refidxs, outJSONFile)
+// prepareCMD 数据文件预处理
+func prepareCMD() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "Pre",
+		Short: "预处理",
+		Long:  "对数据库文件进行预处理得到注释所需文件",
+		Run: func(cmd *cobra.Command, args []string) {
+			referenceChan := make(chan data.Fasta, 0)
+			go data.ReadFastaFile(path.Join(Param.DBPath, data.Config.DBFile.Reference), referenceChan)
+			refgenesChan := make(chan data.Refgenes, 0)
+			refgeneFiles := []string{path.Join(Param.DBPath, data.Config.DBFile.Refgene), path.Join(Param.DBPath, data.Config.DBFile.EnsMt)}
+			go data.ReadRefgeneFiles(refgeneFiles, refgenesChan)
+			refidxMapChan := make(chan map[string]data.Refidxs, 0)
+			go data.InitRefidxMap(refidxMapChan)
+			refgenes := <-refgenesChan
+			data.WriteMrnaFile(path.Join(Param.DBPath, data.Config.DBFile.Mrna), refgenes, <-referenceChan)
+			data.WriteRefidxFile(path.Join(Param.DBPath, data.Config.DBFile.Refidx), <-refidxMapChan, refgenes.ToChromMap())
+		},
 	}
+	cmd.Flags().StringVarP(&Param.DBPath, "config", "c", "config.yml", "配置文件")
+	cmd.Flags().StringVarP(&Param.DBPath, "db_path", "d", "humandb", "数据库文件目录")
+	return cmd
+}
+
+// annoGATKSNVCMD 注释GATK4 Call SNV的VCF结果文件
+func annoGATKSNVCMD() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "SNV",
+		Short: "SNV注释(GATK4)",
+		Long:  "注释GATK4 Call SNV的VCF结果文件",
+		Run: func(cmd *cobra.Command, args []string) {
+			ncbiGeneChan := make(chan data.NcbiGene, 0)
+			go data.ReadNCBIGeneInfo(path.Join(Param.DBPath, data.Config.DBFile.NcbiGene), ncbiGeneChan)
+			refgenesChan := make(chan data.Refgenes, 0)
+			refgeneFiles := []string{path.Join(Param.DBPath, data.Config.DBFile.Refgene), path.Join(Param.DBPath, data.Config.DBFile.EnsMt)}
+			go data.ReadRefgeneFiles(refgeneFiles, refgenesChan)
+			mrnaChan := make(chan data.Fasta, 0)
+			go data.ReadFastaFile(path.Join(Param.DBPath, data.Config.DBFile.Mrna), mrnaChan)
+			refidxsChan := make(chan data.Refidxs)
+			go data.ReadRefidxFile(path.Join(Param.DBPath, data.Config.DBFile.Refidx), refidxsChan)
+			refgenes := <-refgenesChan
+			refgenes.SetEntrezidAndSequence(<-ncbiGeneChan, <-mrnaChan)
+			gatkSnvsChan := make(chan snv.Snvs)
+			go snv.ReadGatkVcfFile(Param.Input, gatkSnvsChan)
+			snv.RunAnnotation(<-gatkSnvsChan, refgenes.ToSnMap(), <-refidxsChan, Param.Ouput)
+		},
+	}
+	cmd.Flags().StringVarP(&Param.Config, "config", "c", "config.yml", "配置文件")
+	cmd.Flags().StringVarP(&Param.DBPath, "db_path", "d", "humandb", "数据库文件目录")
+	cmd.Flags().StringVarP(&Param.Input, "input", "i", "input.vcf", "输入vcf文件")
+	cmd.Flags().StringVarP(&Param.Ouput, "output", "o", "output.json", "输出JSON文件")
+	cmd.Flags().IntVarP(&Param.SplicingLength, "splicing_len", "s", -1, "预定义的剪接区域长度")
+	return cmd
+}
+
+// annoXHMMCNVCMD 注释XHMM Call CNV的VCF结果文件
+func annoXHMMCNVCMD() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "CNV",
+		Short: "CNV注释(XHMM)",
+		Long:  "注释XHMM Call CNV的VCF结果文件",
+		Run: func(cmd *cobra.Command, args []string) {
+			ncbiGeneChan := make(chan data.NcbiGene, 0)
+			go data.ReadNCBIGeneInfo(path.Join(Param.DBPath, data.Config.DBFile.NcbiGene), ncbiGeneChan)
+			refgenesChan := make(chan data.Refgenes, 0)
+			refgeneFiles := []string{path.Join(Param.DBPath, data.Config.DBFile.Refgene), path.Join(Param.DBPath, data.Config.DBFile.EnsMt)}
+			go data.ReadRefgeneFiles(refgeneFiles, refgenesChan)
+			refidxsChan := make(chan data.Refidxs)
+			go data.ReadRefidxFile(path.Join(Param.DBPath, data.Config.DBFile.Refidx), refidxsChan)
+			xhmmCnvMapChan := make(chan map[string]cnv.Cnvs, 0)
+			go cnv.ReadXhmmVcfFile(Param.Input, xhmmCnvMapChan)
+			refgenes := <-refgenesChan
+			refidxs := <-refidxsChan
+			for sample, cnvs := range <-xhmmCnvMapChan {
+				outJSONFile := path.Join(Param.Ouput + "." + sample + ".json")
+				cnv.RunAnnotation(cnvs, refgenes.ToSnMap(), refidxs, outJSONFile)
+			}
+		},
+	}
+	cmd.Flags().StringVarP(&Param.Config, "config", "c", "config.yml", "配置文件")
+	cmd.Flags().StringVarP(&Param.DBPath, "db_path", "d", "humandb", "数据库文件目录")
+	cmd.Flags().StringVarP(&Param.Input, "input", "i", "input.vcf", "输入vcf文件")
+	cmd.Flags().StringVarP(&Param.Ouput, "output", "o", "output.json", "输出JSON文件")
+	return cmd
 }
 
 func init() {
-	flag.StringVar(&Param.Program, "p", "gatk_snv", "The Program which is run:[prepare, gatk_snv, xhmm_cnv]")
-	flag.StringVar(&Param.Input, "i", "", "Input File")
-	flag.StringVar(&Param.Ouput, "o", "", "Output File")
-	flag.StringVar(&Param.Config, "c", "", "Config YAML File")
-	flag.StringVar(&Param.DBPath, "d", "./database", "Directory Path of Database")
-	flag.IntVar(&Param.SplicingLength, "s", 15, "Length of Splicing Region")
-	flag.BoolVar(&Param.Help, "h", false, "Help")
-	flag.Parse()
-	if Param.Help {
-		flag.Usage()
-		os.Exit(0)
+	CorbaCMD = &cobra.Command{
+		Use:   "grandanno",
+		Short: "注释",
+		Long:  "变异注释软件",
 	}
-	if len(Param.Input) == 0 || len(Param.Ouput) == 0 || len(Param.Config) == 0 {
-		flag.Usage()
-		os.Exit(-1)
-	}
+	CorbaCMD.AddCommand(prepareCMD(), annoGATKSNVCMD(), annoXHMMCNVCMD())
 	data.ReadConfigYAML(Param.Config)
-	data.Config.Param.SplicingLen = Param.SplicingLength
+	if Param.SplicingLength > 0 {
+		data.Config.Param.SplicingLen = Param.SplicingLength
+	}
 }
 
 func main() {
-	switch Param.Program {
-	case "prepare":
-		runPrepare()
-	case "gatk_snv":
-		runAnnoGatkSnv()
-	case "xhmm_snv":
-		runAnnoXhmmCnv()
-	default:
-		flag.Usage()
-	}
+	CorbaCMD.Execute()
 }
